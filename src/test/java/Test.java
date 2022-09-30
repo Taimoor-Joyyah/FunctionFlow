7ff2641230a84833cc7cb282c39e486899e45ae1
taimoor <mtaimoor0167@gmail.com>
2022-09-30 20:13:03 +0500

    cleaning

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -9,38 +9,4 @@ public class Test {
         var parser = new FlowParser(functionCode);
         parser.parse();
     }
-
-
-//    public void parse(String code) {
-//        int index = 0;
-//        int statementIndex;
-//        int conditionalIndex;
-//        while (index + 1 < code.length()) {
-//            statementIndex = code.indexOf(';', index);
-//            conditionalIndex = code.indexOf('{', index);
-//            if (conditionalIndex >= 0 && conditionalIndex < statementIndex) {
-//                var close = FlowParser.otherEnd(conditionalIndex, code);
-//                if (FlowParser.nextWord(close + 1, code).equals("else")) {
-//                    var open = code.indexOf('{', close);
-//                    close = FlowParser.otherEnd(open, code);
-//                }
-//                var conditionalString = code.substring(index, close + 1);
-//                var conditionalBody = new ConditionBody();
-//                conditionalBody.parse(conditionalString);
-//                bodies.add(conditionalBody);
-//                index = close + 1;
-//            } else {
-//                if (statementIndex >= 0) {
-//                    int close = index;
-//                    statementBlocking(code, index, close);
-//                    index = close + 1;
-//                } else {
-//                    if (code.substring(index).isBlank()) {
-//                        break;
-//                    }
-//                }
-//            }
-//        }
-//    }
-
 }
